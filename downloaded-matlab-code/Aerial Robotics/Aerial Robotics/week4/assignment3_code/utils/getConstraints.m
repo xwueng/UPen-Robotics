3ad56a7051132c2e55946c6ecbc1f3beb150be5f
xwueng <62865730+xwueng@users.noreply.github.com>
2025-03-25 17:10:34 -0700

    Delete downloaded-matlab-code directory

--- downloaded-matlab-code/Aerial Robotics/Aerial Robotics/week4/assignment3_code/utils/getConstraints.m
+++
@@ -1,7 +0,0 @@
-function [constraints] = getConstraints(nrow, waypoints, wpdindex )
-    constraints = zeros(nrow, 1);
-    ncol = size(waypoints, 2);
-    for i = 1:ncol-1
-        constraints(i:i+1) = waypoints(wpdindex, i:i+1)';
-    end % loop i
-end % function getConstraints
--- downloaded-matlab-code/Aerial Robotics/Aerial Robotics/week4/assignment3_code/utils/getPolyEqs.m
+++
@@ -1,9 +0,0 @@
-function PolyEq = getPolyEqs(x, npoly, ndiv, nseg)
-    % create an matrix for polynomical equations, each column corresponds
-    % to a segement, row 1 original polynomical, row 2: 1st order
-    % derivative ...
-
-    sym_coefs = sym('c%d%d' [8 4]);
-    
-end % get PolyEqs
-
